3a0e30eb5a0ff44b750c1534fc73dc5a2c493f15
nastya <gleddders@gmail.com>
2023-10-18 21:08:54 +0400

    Размер массива [#333582 #325513]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,7 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new  float[40];
+
+        System.out.println(ages.length + "\n" + surnames.length + '\n' + prices.length);
     }
 }
